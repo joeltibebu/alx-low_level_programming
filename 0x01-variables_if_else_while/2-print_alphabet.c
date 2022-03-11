4afe2ea634759b122fb5ac3e500bcf6b97ff6277
joeltibebu <ets049610eeng@aastu.edu.et>
2022-03-11 22:31:28 +0300

    second repo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * The function prints out a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+
+putchar('\n');
+
+return (0);
+}
